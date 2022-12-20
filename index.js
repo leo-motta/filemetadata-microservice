2516e17bc74f95d99e5d88524d04a86a63bc4c82
Leonardo Motta <64990810+leo-motta@users.noreply.github.com>
2022-12-20 13:53:56 +0000

    add file upload API route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var multer = require('multer');
 var app = express();
 
 app.use(cors());
@@ -11,10 +11,24 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+//Body Parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//File upload API route
+app.route('/api/fileanalyse').post(multer().single('upfile'), async(req, res) => {
+  try {
+    res.json({
+    'name' : req.file.originalname,
+    'type' : req.file.mimetype,
+    'size' : req.file.size
+   });
+  } catch (err) {
+    console.log(err)
+  }
+})
